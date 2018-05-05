159c016831be2dc5e0879147613f2b9375534f95
Louis Dionne <ldionne.2@gmail.com>
2018-05-05 12:52:11 -0600

    Add missing includes

--- code/inheritance.cpp
+++ code/inheritance.cpp
@@ -2,6 +2,7 @@
 // Distributed under the Boost Software License, Version 1.0.
 
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
 
--- code/shared_remote_storage.cpp
+++ code/shared_remote_storage.cpp
@@ -6,6 +6,7 @@
 #include <cstddef>
 #include <cstdlib>
 #include <iostream>
+#include <memory>
 #include <string>
 #include <vector>
 
